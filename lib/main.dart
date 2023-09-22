ae6a5260d6698a7310eca69aed32659913cd5b3b
Jade.Zhang <zjhiphop@gmail.com>
2023-09-22 09:28:57 +0800

    feat: init

---
+++ lib/main.dart
@@ -0,0 +1,273 @@
+import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:zoomeye/examples/cloudanchorexample.dart';
+import 'package:zoomeye/examples/debugoptionsexample.dart';
+import 'package:zoomeye/examples/externalmodelmangementexample.dart';
+import 'package:zoomeye/examples/localandwebobjectsexample.dart';
+import 'package:zoomeye/examples/objectgestureexample.dart';
+import 'package:zoomeye/examples/objectsonplanesexample.dart';
+import 'package:zoomeye/examples/screenshotexample.dart';
+
+void main() {
+  // runApp(const MyApp());
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  String _platformVersion = 'Unknown';
+  static const String _title = 'AR Plugin Demo';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await ArFlutterPlugin.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(_title),
+          ),
+          body: Column(children: [
+            Text('Running on: $_platformVersion\n'),
+            const Expanded(
+              child: ExampleList(),
+            ),
+          ]),
+        ),
+      );
+    }
+  }
+
+class ExampleList extends StatelessWidget {
+  const ExampleList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final examples = [
+      Example(
+          'Debug Options',
+          'Visualize feature points, planes and world coordinate system',
+          () => Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const DebugOptionsWidget()))),
+      Example(
+          'Local & Online Objects',
+          'Place 3D objects from Flutter assets and the web into the scene',
+          () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const LocalAndWebObjectsWidget()))),
+      Example(
+          'Anchors & Objects on Planes',
+          'Place 3D objects on detected planes using anchors',
+          () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const ObjectsOnPlanesWidget()))),
+      Example(
+          'Object Transformation Gestures',
+          'Rotate and Pan Objects',
+          () => Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ObjectGesturesWidget()))),
+      Example(
+          'Screenshots',
+          'Place 3D objects on planes and take screenshots',
+          () => Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ScreenshotWidget()))),
+      Example(
+          'Cloud Anchors',
+          'Place and retrieve 3D objects using the Google Cloud Anchor API',
+          () => Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const CloudAnchorWidget()))),
+      Example(
+          'External Model Management',
+          'Similar to Cloud Anchors example, but uses external database to choose from available 3D models',
+          () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const ExternalModelManagementWidget())))
+    ];
+    return ListView(
+      children:
+          examples.map((example) => ExampleCard(example: example)).toList(),
+    );
+  }
+}
+
+class ExampleCard extends StatelessWidget {
+  const ExampleCard({Key? key, required this.example}) : super(key: key);
+  final Example example;
+
+  @override
+  build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        onTap: () {
+          example.onTap();
+        },
+        child: ListTile(
+          title: Text(example.name),
+          subtitle: Text(example.description),
+        ),
+      ),
+    );
+  }
+}
+
+class Example {
+  const Example(this.name, this.description, this.onTap);
+  final String name;
+  final String description;
+  final Function onTap;
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
